7e6fba27368605c566fd1ae290783a8ed0694b15
xiehengjian <846188037@qq.com>
2022-11-12 15:43:18 +0800

    url

--- Formula/mn.rb
+++ Formula/mn.rb
@@ -1,7 +1,7 @@
 class DockerConnector < Formula
     desc "MarginNote CLi"
     # homepage ""
-    # url ""
+    url "https://raw.githubusercontent.com/xiehengjian/homebrew-brew/main/Formula/mn.tar.gz"
     sha256 "661a2a10756d6b7fb37fa1f5e57db42fafe25adcdb6418d6e379790c0c557531"
     version "0.0.1"
     def install
