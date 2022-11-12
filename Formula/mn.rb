7d4fbd01f4118046de77cd939c2ec3ede6912e55
xiehengjian <846188037@qq.com>
2022-11-12 15:41:52 +0800

    init

---
+++ Formula/mn.rb
@@ -0,0 +1,10 @@
+class DockerConnector < Formula
+    desc "MarginNote CLi"
+    # homepage ""
+    # url ""
+    sha256 "661a2a10756d6b7fb37fa1f5e57db42fafe25adcdb6418d6e379790c0c557531"
+    version "0.0.1"
+    def install
+      bin.install "mn"
+    end
+  end
